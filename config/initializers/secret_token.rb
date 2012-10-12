506fd2f28bb07d8e72816af02be6034f7e561931
Peter Gadzinski <peter@maxscheduler.com>
2012-10-12 19:47:49 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Users::Application.config.secret_token = 'fe3cd9f715f4f7b46e0277358eec70fe8e6cc0cebea4f6c0983405583c2b8045439d67173c212b38e7be1b45ff039f35943631e8d1642b11825c171c6ea0907a'
